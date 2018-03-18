696c0f01e5456eb47251714939bb0a12624dc784
flopeters1337 <fl0wryan@hotmail.com>
2018-03-18 18:15:35 +0100

    Add client handling on server side

--- src/server.py
+++ src/server.py
@@ -6,30 +6,56 @@ from threading import Thread
 logging.basicConfig(level=logging.INFO)
 
 PORT_NUMBER = 1337
+BUFFER_SIZE = 4096
 
 
 def handle_client(client_socket):
-    pass
+    msg = b''
+
+    try:
+        while True:
+            chunk = client_socket.recv(BUFFER_SIZE)
+            if chunk is b'':
+                raise RuntimeError('Socket connection broken')
+            msg += chunk
+
+            # If we have received an End Of String token
+            if ':EOS:' in chunk.decode('utf-8'):
+                base64_string = msg[:(len(msg) - 5)]
+                string = base64.b64decode(base64_string).decode('utf-8')
+
+                # Handle message
+                logging.info('Received message "' + string + '"')
+
+                # Reset message buffer
+                msg = b''
+    finally:
+        client_socket.close()
 
 
 if __name__ == '__main__':
     # Initialize server socket
     logging.info('Initializing server')
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((socket.gethostname(), PORT_NUMBER))
+    hostname = socket.gethostname()
+    server_socket.bind((hostname, PORT_NUMBER))
 
     # Listen for connections
-    logging.info('Listening on port ' + str(PORT_NUMBER))
+    logging.info('Listening on port ' + str(PORT_NUMBER) + ' with hostname ' +
+                 hostname)
     server_socket.listen(5)
 
     # Main loop
-    while True:
-        try:
-            logging.info('Waiting for requests')
-            (client_socket, address) = server_socket.accept()
-            logging.info('Received client connection')
-            ct = Thread(target=handle_client, args=client_socket)
-            ct.run()
-        except Exception as e:
-            logging.error('Caught ' + str(type(e)) + ': "' + str(e)
-                          + '" Now recovering')
+    try:
+        while True:
+            try:
+                logging.info('Waiting for requests')
+                (client_socket, address) = server_socket.accept()
+                logging.info('Received client connection')
+                ct = Thread(target=handle_client, args=[client_socket])
+                ct.run()
+            except Exception as e:
+                logging.error(type(e).__name__ + ': "' + str(e)
+                              + '" Now recovering')
+    finally:
+        server_socket.close()
